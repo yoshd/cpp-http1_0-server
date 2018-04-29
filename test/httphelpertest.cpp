ff04248d0b905009b0a902cd4d40eb8d62bca545
yoshd <garlic.ba.0129@gmail.com>
2018-04-29 17:12:53 +0900

    Fix test case

--- test/httphelpertest.cpp
+++ test/httphelpertest.cpp
@@ -7,7 +7,7 @@ TEST(HTTPHelperTest, TestGetHtml) {
     std::string html_index1 = HTTPHelper::get_content("/index.html");
     std::string html_index2 = HTTPHelper::get_content("/");
     std::string html_index3 = HTTPHelper::get_content("/favicon.ico");
-    std::string expect_index = "<!DOCTYPE html >\n<html lang=\"ja\">\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">\n<title>HelloWorld</title>\n</head>\n<body>\n<h1>Hello, world!</h1>\n<p>こんにちは、世界!</p>\n</body>\n</html>\n";
+    std::string expect_index = "<!DOCTYPE html >\n<html lang=\"ja\">\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"styles/style.css\">\n<title>HelloWorld</title>\n</head>\n<body>\n<h1>Hello, world!</h1>\n<p>こんにちは、世界!</p>\n</body>\n</html>\n";
     EXPECT_EQ(html_index1, expect_index);
     EXPECT_EQ(html_index2, expect_index);
     EXPECT_EQ(html_index3, expect_index);
